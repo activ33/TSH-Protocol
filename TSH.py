aee7a41f006f9eb584eaf9f092c6d685467d58c6
active <66892517+activ33@users.noreply.github.com>
2023-09-20 01:29:10 +0530

    Update TSH.py

--- TSH.py
+++ TSH.py
@@ -10,7 +10,7 @@ import os
 ### SETTINGS ###
 
 # BOT API TOKEN
-apiKey  = "YOUR-BOT'S-API-TOKEN"
+apiKey  = "6344846447:AAH9lv46yedRE3ggY0PeWliY4XgWk0hg01g"
 
 # /START COMMAND
 def start(bot, update):
